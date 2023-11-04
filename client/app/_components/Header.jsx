c2079c96d901125b4242103b1d0b9420a0648cb3
hyoaru <jjcabreraaaa@gmail.com>
2023-11-05 05:08:39 +0800

    [modify] icon sizes

--- client/app/_components/Header.jsx
+++ client/app/_components/Header.jsx
@@ -1,7 +1,7 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@components/ui/dropdown-menu"
 import { Button } from "@components/ui/button"
 import Link from "next/link"
-import { AlignLeft, Search, ShoppingCart } from "lucide-react"
+import { AlignLeft, Search, ShoppingCart, User, Lock } from "lucide-react"
 
 const navigation = [
   { name: 'Collection', link: '/' },
@@ -19,22 +19,25 @@ export default function Header() {
             <div id="main-nav-start" className="flex justify-start items-center w-1/6 xl:w-1/3">
               <div id="main-nav-start-expanded" className="hidden xl:flex">
                 {navigation.map((nav, index) => (
-                  <Button variant={'link'} key={`MainNavStartLinkExpanded-${index}`} className={'px-3'}>
-                    <Link href={nav.link} className="uppercase">{nav.name}</Link>
+                  <Button variant={'link'} key={`MainNavStartLinkExpanded-${index}`} className={'px-2'}>
+                    <Link href={nav.link} className="uppercase font-light">{nav.name}</Link>
                   </Button>
                 ))}
               </div>
               <div id="main-nav-start-collapsed" className="flex items-center xl:hidden">
                 <DropdownMenu>
                   <DropdownMenuTrigger>
-                    <AlignLeft />
+                    <AlignLeft size={17} />
                   </DropdownMenuTrigger>
                   <DropdownMenuContent side={'bottom'} align={'start'}>
-                    <DropdownMenuLabel className={'text-muted-foreground font-normal uppercase'}>Navigation</DropdownMenuLabel>
+                    <DropdownMenuLabel className={'uppercase flex gap-2 items-center'}>
+                      <Search size={17} />
+                      <span className="text-muted-foreground font-light">Search</span>
+                    </DropdownMenuLabel>
                     <DropdownMenuSeparator />
                     {navigation.map((nav, index) => (
                       <DropdownMenuItem key={`MainNavStartLinkCollapsed-${index}`}>
-                        <Link href={nav.link} className="uppercase">{nav.name}</Link>
+                        <Link href={nav.link} className="uppercase font-light">{nav.name}</Link>
                       </DropdownMenuItem>
                     ))}
                   </DropdownMenuContent>
@@ -46,20 +49,23 @@ export default function Header() {
               <Link href={'/'} className="flex uppercase text-lg sm:hidden md:text-2xl">RM Collection</Link>
             </div>
             <div id="main-nav-end" className="flex justify-end items-center w-1/6 xl:w-1/3">
-              <div id="main-nav-end-expanded" className="hidden md:flex">
-                <Button variant={'link'} className={'px-3'}>
-                  <Link href={"/"} className="uppercase">Search</Link>
+              <div id="main-nav-end-expanded" className="hidden xl:flex">
+                <Button variant={'link'} className={'px-2'}>
+                  <Link href={"/"} className="uppercase font-light">Search</Link>
                 </Button>
-                <Button variant={'link'} className={'px-3'}>
-                  <Link href={"/"} className="uppercase">Cart</Link>
+                <Button variant={'link'} className={'px-2'}>
+                  <Link href={"/"} className="uppercase font-light">Cart</Link>
+                </Button>
+                <Button variant={'link'} className={'px-2'}>
+                  <Link href={"/"} className="uppercase font-light">Login</Link>
                 </Button>
               </div>
-              <div id="main-nav-end-collapsed" className="flex md:hidden">
+              <div id="main-nav-end-collapsed" className="flex xl:hidden">
                 <Button variant={'ghost'} className={'px-1'}>
-                  <Link href={"/"} className=""><Search /></Link>
+                  <Link href={"/"} className=""><ShoppingCart size={17} /></Link>
                 </Button>
                 <Button variant={'ghost'} className={'px-1'}>
-                  <Link href={"/"} className=""><ShoppingCart /></Link>
+                  <Link href={"/"} className=""><User size={17} /></Link>
                 </Button>
               </div>
             </div>
