86d39ce51a112ce4cc30caddd1d2f8f8bab56f9c
hyoaru <jjcabreraaaa@gmail.com>
2023-11-08 11:51:47 +0800

    [add] admin route

---
+++ client/app/(routes)/admin/layout.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Link from 'next/link'
+
+// App imports
+import { Button } from '@components/ui/button'
+import { Separator } from '@components/ui/separator'
+
+
+export default function Layout({ children }) {
+  const basePath = '/admin'
+  const adminOperationsBasePath = `${basePath}/operations`
+  const adminTablesBasePath = `${basePath}/tables`
+
+  const navigationOperations = [
+    { name: 'Add product', link: `${adminOperationsBasePath}/add-product` },
+    { name: 'Add sub-admin', link: `${adminOperationsBasePath}/add-sub-admin` },
+  ]
+  const navigationTables = [
+    { name: 'Admins', link: `${adminTablesBasePath}/admins` },
+    { name: 'Users', link: `${adminTablesBasePath}/users` },
+    { name: 'Products', link: `${adminTablesBasePath}/products` },
+    { name: 'Product postings', link: `${adminTablesBasePath}/product-postings` },
+    { name: 'Product variants', link: `${adminTablesBasePath}/product-variants` },
+  ]
+
+  return (
+    <>
+      <div className="md:container mx-auto px-4 mt-4">
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-12 sm:col-span-3 lg:col-span-2">
+            <div className="flex flex-col gap-2">
+              <div className="bg-primary p-2 flex flex-row overflow-x-auto rounded-lg gap-2 sm:flex-col">
+                <small className="text-center uppercase text-xs mb-1 text-primary-foreground hidden sm:block">Operations</small>
+                {navigationOperations.map((navigationOperation) => (
+                  <Button size={'sm'} variant={'secondary'} >
+                    <Link href={navigationOperation.link}>{navigationOperation.name}</Link>
+                  </Button>
+                ))}
+              </div>
+              <div className="border border-x-0 p-2 flex flex-row overflow-x-auto rounded-lg gap-2 sm:flex-col">
+                <small className="text-center uppercase text-xs mb-1 hidden sm:block">Tables</small>
+                {navigationTables.map((navigationTable) => (
+                  <Button size={'sm'} >
+                    <Link href={navigationTable.link}>{navigationTable.name}</Link>
+                  </Button>
+                ))}
+              </div>
+
+            </div>
+          </div>
+          <div className="col-span-12 sm:col-span-9 lg:col-span-10">
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
