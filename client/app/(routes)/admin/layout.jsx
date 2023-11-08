1f9f0baab6338d10de666f3527582d3e780e301b
hyoaru <jjcabreraaaa@gmail.com>
2023-11-08 21:02:03 +0800

    [add] add product variant operation route

--- client/app/(routes)/admin/layout.jsx
+++ client/app/(routes)/admin/layout.jsx
@@ -3,8 +3,7 @@ import Link from 'next/link'
 
 // App imports
 import { Button } from '@components/ui/button'
-import { Separator } from '@components/ui/separator'
-
+import { ScrollArea, ScrollBar } from "@components/ui/scroll-area"
 
 export default function Layout({ children }) {
   const basePath = '/admin'
@@ -12,8 +11,9 @@ export default function Layout({ children }) {
   const adminTablesBasePath = `${basePath}/tables`
 
   const navigationOperations = [
-    { name: 'Add product', link: `${adminOperationsBasePath}/add-product` },
     { name: 'Add sub-admin', link: `${adminOperationsBasePath}/add-sub-admin` },
+    { name: 'Add product', link: `${adminOperationsBasePath}/add-product` },
+    { name: 'Add product variant', link: `${adminOperationsBasePath}/add-product` },
   ]
   const navigationTables = [
     { name: 'Admins', link: `${adminTablesBasePath}/admins` },
@@ -27,9 +27,9 @@ export default function Layout({ children }) {
     <>
       <div className="md:container mx-auto px-4 mt-4 mb-8">
         <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-12 sm:col-span-3 lg:col-span-2">
-            <div className="flex flex-col gap-2">
-              <div className="bg-primary p-2 flex flex-row overflow-x-auto rounded-lg gap-2 sm:flex-col">
+          <div className="col-span-12 sm:col-span-4 lg:col-span-2">
+            <div className="flex flex-col gap-0 sm:gap-2">
+              <div className="sm:bg-primary p-3 flex flex-row overflow-x-auto rounded-lg gap-2 sm:flex-col">
                 <small className="text-center uppercase text-xs mb-1 text-primary-foreground hidden sm:block">Operations</small>
                 {navigationOperations.map((navigationOperation) => (
                   <Button size={'sm'} variant={'secondary'} >
@@ -37,7 +37,7 @@ export default function Layout({ children }) {
                   </Button>
                 ))}
               </div>
-              <div className="border border-x-0 p-2 flex flex-row overflow-x-auto rounded-lg gap-2 sm:flex-col">
+              <div className="sm:border sm:border-x-0 p-3 flex flex-row overflow-x-auto rounded-lg gap-2 sm:flex-col">
                 <small className="text-center uppercase text-xs mb-1 hidden sm:block">Tables</small>
                 {navigationTables.map((navigationTable) => (
                   <Button size={'sm'} >
@@ -48,7 +48,7 @@ export default function Layout({ children }) {
 
             </div>
           </div>
-          <div className="col-span-12 sm:col-span-9 lg:col-span-10">
+          <div className="col-span-12 sm:col-span-8 lg:col-span-10">
             {children}
           </div>
         </div>
