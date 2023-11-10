e243bd512288c8031308892a1bf34035dc0b8306
hyoaru <jjcabreraaaa@gmail.com>
2023-11-10 18:38:44 +0800

    [add] filename sanitation with nanoid

--- client/app/_services/shared/uploadProductImages.js
+++ client/app/_services/shared/uploadProductImages.js
@@ -1,14 +1,16 @@
 import { getBrowserClient } from "@services/supabase/getBrowserClient"
+import { nanoid } from "nanoid"
 
 export default async function uploadProductImages({ images, productCategory, productId, variantId }) {
   const supabase = getBrowserClient()
   const response = { data: null, error: null }
 
   for await (const imageFile of images) {
+    const imageFileName = `${nanoid()}-${imageFile.name}`
     const { data, error } = await supabase
       .storage
       .from('products')
-      .upload(`${productCategory}/${productId}/${variantId}/${imageFile.name}`, imageFile, {
+      .upload(`${productCategory}/${productId}/${variantId}/${imageFileName}`, imageFile, {
         cacheControl: '3600',
         upsert: false
       })
