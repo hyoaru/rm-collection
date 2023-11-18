3138337534da1787a26e25641435412024df9a64
hyoaru <jjcabreraaaa@gmail.com>
2023-11-18 11:12:00 +0800

    [add] global filter

--- client/app/_components/admin/tables/shared/DataTable.jsx
+++ client/app/_components/admin/tables/shared/DataTable.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 // App imports
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@components/ui/table"
+import { Input } from '@components/ui/input'
 import { Button } from '@components/ui/button'
 import useGetTable from '@hooks/admin/operations/useGetTable'
 import { formatTimestampTable } from '@lib/formatTimestamp'
@@ -11,9 +12,9 @@ import { formatTimestampTable } from '@lib/formatTimestamp'
 export default function DataTable(props) {
   const { data, columnDefinition } = props
 
-  columnDefinition.push (
-    { accessorFn: (row) => formatTimestampTable(row.created_at), header: 'created_at' }, 
-    { accessorFn: (row) => formatTimestampTable(row.created_at), header: 'updated_at' }, 
+  columnDefinition.push(
+    { accessorFn: (row) => formatTimestampTable(row.created_at), header: 'created_at' },
+    { accessorFn: (row) => formatTimestampTable(row.created_at), header: 'updated_at' },
   )
 
   const { table, globalFilter, setGlobalFilter, pageRef, flexRender } = useGetTable({
@@ -21,8 +22,23 @@ export default function DataTable(props) {
     columnDefinition: columnDefinition
   })
 
+  function onGlobalFilterChange(event) {
+    setGlobalFilter(event.target.value)
+  }
+
   return (
     <>
+      <div className="flex flex-col sm:flex-row mb-4 gap-2">
+        <Input
+          type="text"
+          placeholder="Filter table"
+          className="w-full sm:w-1/2 me-auto"
+          value={globalFilter}
+          onChange={onGlobalFilterChange}
+        />
+        <Button variant={'secondary'}>Export to CSV</Button>
+      </div >
+
       <Table>
         <TableHeader className={'bg-secondary'}>
           {table.getHeaderGroups().map((headerGroup) => (
