46ba95ddf3302ee9527311abd9abd30dddc0851c
hyoaru <jjcabreraaaa@gmail.com>
2023-11-19 15:17:48 +0800

    [add] admin precedence check

--- client/app/_services/admin/tables/removeAdminAuthority.js
+++ client/app/_services/admin/tables/removeAdminAuthority.js
@@ -1,20 +1,46 @@
 "use server"
 
 import { getServerClient } from "@services/supabase/getServerClient"
+import { ADMIN_ROLES } from "@constants/admin"
 
 export default async function removeAdminAuthority(userId) {
   const supabase = getServerClient()
 
   const { data: { user: userStateAuth } } = await supabase.auth.getUser()
-  if (userId === userStateAuth.id) {
-    return { error: { message: "Operation must be done by someone of higher authority." } }
-  }
+  let { data: userStateGeneral } = await supabase
+    .from('users')
+    .select("*")
+    .eq('id', userStateAuth?.id)
+    .single()
 
   const { data, error } = await supabase
     .from('users')
-    .update({ role: 'user' })
+    .select('*')
     .eq('id', userId)
-    .select()
+    .single()
+    .then(async ({ data: getUserInfoData, error: getUserInfoError }) => {
+      if (getUserInfoError) {
+        return { getUserInfoData, getUserInfoError }
+      }
+
+      const currentAdminAuthorityPrecedencePosition = ADMIN_ROLES.findIndex((adminRole) => adminRole === userStateGeneral.role)
+      const targetAdminAuthorityPrecedencePosition = ADMIN_ROLES.findIndex((adminRole) => adminRole === getUserInfoData.role)
+
+      if (currentAdminAuthorityPrecedencePosition >= targetAdminAuthorityPrecedencePosition) {
+        return {
+          data: null,
+          error: { message: "Operation must be done by someone of higher authority." }
+        }
+      }
+
+      const { data, error } = await supabase
+        .from('users')
+        .update({ role: 'user' })
+        .eq('id', userId)
+        .select()
+
+      return { data, error }
+    })
 
   return { data, error }
 }
