f3e459e78bb0638b60b5eb208348104fa0115035
hyoaru <jjcabreraaaa@gmail.com>
2023-11-19 20:43:59 +0800

    [add] first name and last name field on sign up

--- client/app/_components/auth/SignUpForm.jsx
+++ client/app/_components/auth/SignUpForm.jsx
@@ -22,7 +22,10 @@ export default function SignUpForm() {
   const formSchema = z.object({
     email: z.string().trim().toLowerCase().email().min(8),
     password: z.string().trim().min(8).max(40),
-    confirmPassword: z.string().trim().min(8).max(40)
+    confirmPassword: z.string().trim().min(8).max(40),
+    firstName: z.string().trim().min(2).max(100),
+    lastName: z.string().trim().min(2).max(100),
+
   }).superRefine(({ confirmPassword, password }, ctx) => {
     if (confirmPassword !== password) {
       ctx.addIssue({
@@ -38,24 +41,41 @@ export default function SignUpForm() {
     defaultValues: {
       email: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      firstName: "",
+      lastName: "",
     }
   })
 
   async function onSubmit(data) {
-    await userSignUp({ email: data.email, password: data.password })
+    await userSignUp({
+      email: data.email,
+      password: data.password,
+      firstName: data.firstName,
+      lastName: data.lastName
+    })
       .then(({ data, error }) => {
         if (!error) {
           toast({
             title: "Confirmation email has been sent!",
             description: "Please confirm account creation."
           })
+
+          form.reset()
         } else {
-          toast({
-            variant: "destructive",
-            title: "An error has occured.",
-            description: "Please try again later."
-          })
+          if (error.status === 400) {
+            toast({
+              variant: "destructive",
+              title: "Email is already taken.",
+              description: "Please register with another email."
+            })
+          } else {
+            toast({
+              variant: "destructive",
+              title: "An error has occured.",
+              description: "Please try again later."
+            })
+          }
         }
       })
   }
@@ -64,6 +84,34 @@ export default function SignUpForm() {
     <>
       <Form {...form}>
         <form action="" onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
+          <FormField
+            control={form.control}
+            name={'firstName'}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder={'your-first-name'} {...field} />
+                </FormControl>
+                <FormMessage className={'text-center text-xs'} />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name={'lastName'}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder={'your-last-name'} {...field} />
+                </FormControl>
+                <FormMessage className={'text-center text-xs'} />
+              </FormItem>
+            )}
+          />
+
+          <Separator />
+
           <FormField
             control={form.control}
             name={'email'}
--- client/app/_hooks/authentication/useUserSignUp.js
+++ client/app/_hooks/authentication/useUserSignUp.js
@@ -5,13 +5,26 @@ export function useUserSignUp() {
   const [isLoading, setIsLoading] = useState(false)
   const supabase = getBrowserClient()
 
-  async function userSignUp({ email, password }) {
+  async function userSignUp({ email, password, firstName, lastName }) {
     setIsLoading(true)
-    let { data, error } = await supabase.auth.signUp({
+    const { data, error } = await supabase.auth.signUp({
       email: email,
       password: password,
-      options: { emailRedirectTo: "http://127.0.0.1:3000/auth/sign-in" }
+      options: { emailRedirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/sign-in` }
     })
+      .then(async ({ data: authSignUpData, error: authSignUpError }) => {
+        if (authSignUpError) {
+          return { data: authSignUpData, error: authSignUpError }
+        }
+
+        const { data, error } = await supabase
+          .from('users')
+          .update({ first_name: firstName, last_name: lastName })
+          .eq('id', authSignUpData.user.id)
+          .select()
+          
+        return { data, error }
+      })
 
     setIsLoading(false)
     return { data, error }
