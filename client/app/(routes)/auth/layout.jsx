74906228c52eadac804679f96d5937f923e2d482
hyoaru <jjcabreraaaa@gmail.com>
2023-11-20 12:17:11 +0800

    [add] bottom margin

--- client/app/(routes)/auth/layout.jsx
+++ client/app/(routes)/auth/layout.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Layout({ children }) {
   return (
     <>
-      <div className="px-4 container">
+      <div className="px-4 container mb-8">
         <div className="mx-auto mt-10 sm:mt-16 w-11/12 sm:w-9/12 md:w-7/12">
           {children}
         </div>
