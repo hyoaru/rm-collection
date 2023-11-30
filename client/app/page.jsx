88cd54fa65ac5de81dfdbcb0e816df31f128558a
hyoaru <jjcabreraaaa@gmail.com>
2023-12-01 05:09:08 +0800

    [modify] image ttl -> 60

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,15 +1,13 @@
 import React from "react"
-import Image from "next/image"
 
 // App imports
 import getCollection from "@services/main/getCollection"
 import ProductsFeed from "@components/shared/ProductsFeed"
-import revalidateAllData from "./_services/shared/revalidateAllData"
 
 export default async function Home() {
   const { data: products, error } = await getCollection()
   const breadcrumbs = [{ label: "Collection", link: "/" }]
-  const inStock = 3
+  const inStock = products.length
 
   return (
     <>
