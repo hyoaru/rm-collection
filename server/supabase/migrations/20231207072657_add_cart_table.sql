322ec61a656a31b46fa6babe6a684fce8d3a9aa3
hyoaru <jjcabreraaaa@gmail.com>
2023-12-07 15:37:34 +0800

    [add] cart table

---
+++ server/supabase/migrations/20231207072657_add_cart_table.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS public.cart CASCADE;
+
+CREATE TABLE IF NOT EXISTS cart (
+  id UUID DEFAULT extensions.uuid_generate_v4() PRIMARY KEY,
+  user_id UUID REFERENCES public.users NOT NULL,
+  product_variant_id UUID REFERENCES public.product_variants NOT NULL,
+  quantity NUMERIC DEFAULT(1),
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+-- Policies
+
+ALTER TABLE IF EXISTS public.cart ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Allow read operation for cart table based on user id" ON "public"."cart"
+AS PERMISSIVE FOR SELECT
+TO authenticated
+USING (auth.uid() = id);
+
+CREATE POLICY "Allow update operation for cart based on id" ON "public"."cart"
+AS PERMISSIVE FOR UPDATE
+TO authenticated
+USING (auth.uid() = user_id)
+WITH CHECK (auth.uid() = user_id);
+
+CREATE POLICY "Allow delete operation for cart based on id" ON "public"."cart"
+AS PERMISSIVE FOR DELETE
+TO authenticated
+USING (auth.uid() = user_id);
+
+-- Procedures
+
+CREATE EXTENSION IF NOT EXISTS moddatetime schema extensions;
+
+CREATE TRIGGER handle_updated_at BEFORE UPDATE ON public.cart
+  FOR EACH ROW EXECUTE PROCEDURE moddatetime (updated_at);
