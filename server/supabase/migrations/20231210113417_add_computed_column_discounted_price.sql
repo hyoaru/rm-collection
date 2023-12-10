cce09092baf2e198fd38fb0b312ef3ef7332dbf6
hyoaru <jjcabreraaaa@gmail.com>
2023-12-10 19:34:54 +0800

    [add] computed column discounted price

---
+++ server/supabase/migrations/20231210113417_add_computed_column_discounted_price.sql
@@ -0,0 +1,7 @@
+ALTER TABLE IF EXISTS public.product_variants
+ADD COLUMN discounted_price NUMERIC GENERATED ALWAYS AS (
+  CASE
+    WHEN discount_rate = 0 THEN price
+    ELSE price - (price * (discount_rate / 100))
+  END
+) STORED;
