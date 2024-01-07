e723342c36e3099edd7ffa70087ce0b41fb6ee5d
hyoaru <jjcabreraaaa@gmail.com>
2024-01-07 19:00:05 +0800

    [fix] product images unable to be readwhen not authenticated

--- server/supabase/migrations/20231110095645_product_bucket_allow_all_operations_admin.sql
+++ server/supabase/migrations/20231110095645_product_bucket_allow_all_operations_admin.sql
@@ -1,7 +1,19 @@
-CREATE POLICY "Allow all operations to main admin 1ifhysk_0" ON storage.objects FOR UPDATE TO public USING (bucket_id = 'products' AND auth.role() = 'authenticated');
+CREATE POLICY "Allow all operations to main admin 1ifhysk_0" ON storage.objects 
+FOR UPDATE 
+TO public 
+USING (bucket_id = 'products' AND auth.role() = 'authenticated');
 
-CREATE POLICY "Allow all operations to main admin 1ifhysk_1" ON storage.objects FOR DELETE TO public USING (bucket_id = 'products' AND auth.role() = 'authenticated');
+CREATE POLICY "Allow all operations to main admin 1ifhysk_1" ON storage.objects 
+FOR DELETE 
+TO public 
+USING (bucket_id = 'products' AND auth.role() = 'authenticated');
 
-CREATE POLICY "Allow all operations to main admin 1ifhysk_2" ON storage.objects FOR INSERT TO public WITH CHECK (bucket_id = 'products' AND auth.role() = 'authenticated');
+CREATE POLICY "Allow all operations to main admin 1ifhysk_2" ON storage.objects 
+FOR INSERT 
+TO public 
+WITH CHECK (bucket_id = 'products' AND auth.role() = 'authenticated');
 
-CREATE POLICY "Allow all operations to main admin 1ifhysk_3" ON storage.objects FOR SELECT TO public USING (bucket_id = 'products' AND auth.role() = 'authenticated');
+CREATE POLICY "Allow all operations to main admin 1ifhysk_3" ON storage.objects 
+FOR SELECT 
+TO public 
+USING (bucket_id = 'products' AND auth.role() = 'authenticated');
---
+++ server/supabase/migrations/20240107105242_allow_read_operation_to_everyone.sql
@@ -0,0 +1,4 @@
+CREATE POLICY "Allow read operation to everyone 1ifhysk_0" 
+ON storage.objects FOR SELECT 
+TO public 
+USING (bucket_id = 'products');
