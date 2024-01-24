4711b744fb2b286f3614e27796c0ede158c71d34
hyoaru <jjcabreraaaa@gmail.com>
2024-01-24 13:14:49 +0800

    [fix] delete user not working

--- client/app/_services/admin/tables/deleteUser.js
+++ client/app/_services/admin/tables/deleteUser.js
@@ -2,10 +2,18 @@
 
 // App imports
 import processErrorToCrossSideSafe from "@/app/_lib/processErrorToCrossSideSafe"
-import { getServerClient } from "@services/supabase/getServerClient"
+import { createClient } from "@supabase/supabase-js"
 
 export default async function deleteUser(userId) {
-  const supabase = await getServerClient()
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY, {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false
+      }
+    }
+  )
+
   const { data, error } = await supabase.auth.admin.deleteUser(userId)
 
   return { data, error: processErrorToCrossSideSafe(error) }
