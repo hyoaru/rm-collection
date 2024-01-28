c26e83bd89812d260d29829b50e4398a4803a195
hyoaru <jjcabreraaaa@gmail.com>
2024-01-29 07:06:33 +0800

    [fix] syntax error fucntion

--- server/supabase/migrations/20231106051010_initial_config.sql
+++ server/supabase/migrations/20231106051010_initial_config.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 ALTER TABLE IF EXISTS public.users ENABLE ROW LEVEL SECURITY;
 
-CREATE OR REPLACE FUCNTION public.handle_new_user() 
+CREATE OR REPLACE FUNCTION public.handle_new_user() 
 RETURNS trigger AS $$
 BEGIN
   INSERT INTO public.users (id, email)
