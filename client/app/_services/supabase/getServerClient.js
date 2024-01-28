3714e8e1037db648dd508e5366de3e7057f5e04e
hyoaru <jjcabreraaaa@gmail.com>
2024-01-29 06:36:33 +0800

    [fix] dynamic server error caused by cookies

--- client/app/_services/supabase/getServerClient.js
+++ client/app/_services/supabase/getServerClient.js
@@ -4,6 +4,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
 export async function getServerClient() {
-  const supabase = createServerComponentClient({ cookies })
+  const cookieStore = cookies()
+  const supabase = createServerComponentClient({ cookies: () => cookieStore })
   return supabase
 }
