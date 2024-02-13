f34e71a1d38221a82e89a39caeeb943a5c2bd4c7
hyoaru <jjcabreraaaa@gmail.com>
2024-02-13 17:17:00 +0800

    [add] allow insert operation for admin tier 2

---
+++ server/supabase/migrations/20240213085511_allow_create_operation_on_admin_tier_2.sql
@@ -0,0 +1,22 @@
+-- Products
+DROP POLICY IF EXISTS "Allow insert operation for tier 2 admin on products table" ON public.products;
+CREATE POLICY "Allow insert operation for tier 2 admin on products table"
+ON public.products
+FOR INSERT TO authenticated WITH CHECK (
+  (SELECT role FROM public.users WHERE id = auth.uid() LIMIT 1) = 'admin_tier_2'
+);
+
+-- Product variants
+DROP POLICY IF EXISTS "Allow insert operation for tier 2 admin on product variants table" ON public.products;
+CREATE POLICY "Allow insert operation for tier 2 admin on product variants table"
+ON public.product_variants
+FOR INSERT TO authenticated WITH CHECK (
+  (SELECT role FROM public.users WHERE id = auth.uid() LIMIT 1) = 'admin_tier_2'
+);
+
+-- Buckets
+DROP POLICY IF EXISTS "Allow insert operation to admin tier 2" ON storage.objects ;
+CREATE POLICY "Allow insert operation to admin tier 2" ON storage.objects 
+FOR INSERT 
+TO public 
+WITH CHECK (bucket_id = 'products' AND auth.role() = 'authenticated');
