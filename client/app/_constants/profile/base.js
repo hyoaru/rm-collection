f683e57fb195a99bd7035f16e89d562cc181f8ed
hyoaru <jjcabreraaaa@gmail.com>
2024-02-17 16:01:53 +0800

    [migrate] to typescript

--- client/app/_constants/profile/base.js
+++
@@ -1,8 +0,0 @@
-export const PROFILE_BASE_PATH = 'profile'
-export const ACCOUNT_BASE_PATH = `${PROFILE_BASE_PATH}/account`
-
-export const NAVIGATIONS = [
-  { name: 'Account information', pathName: `/${PROFILE_BASE_PATH}` },
-  { name: 'Update information', pathName: `/${ACCOUNT_BASE_PATH}/update-information` },
-  { name: 'Update email', pathName: `/${ACCOUNT_BASE_PATH}/update-email` },
-]
--- client/app/_lib/getDiscountedPrice.js
+++
@@ -1,6 +0,0 @@
-export default function getDiscountedPrice({ originalPrice, discountRate }) {
-  if (discountRate <= 0) { return originalPrice }
-  const discount = originalPrice * (discountRate / 100)
-  const discountedPrice = originalPrice - discount
-  return discountedPrice
-}
