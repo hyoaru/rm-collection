cf330e3a9cafce1787ff6b25f3e4f1ab397ed5d5
hyoaru <jjcabreraaaa@gmail.com>
2024-02-22 19:02:38 +0800

    [remove] optimistic update in favor of only implementing it for visual of immediate use

--- client/app/_hooks/admin/operations/useUpdateProduct.ts
+++ client/app/_hooks/admin/operations/useUpdateProduct.ts
@@ -4,39 +4,12 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 // App imports
 import { AsyncReturnType } from "@constants/shared/types";
 import updateProductWithThumbnail from "@services/admin/operations/updateProductWithThumbnail";
-import { ProductsResponseType, ProductResponseType } from "@constants/base/types";
-import updateObjectValues from "@lib/updateObjectValues";
 
-export function useUpdateProduct(formSchema: z.ZodObject<any>) {
+export function useUpdateProduct() {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: updateProductWithThumbnail,
-    onMutate: async (response: z.infer<typeof formSchema>) => {
-      await queryClient.cancelQueries({ queryKey: ['products'] })
-      await queryClient.cancelQueries({ queryKey: ['product', response.id] })
-
-      const previousProduct = queryClient.getQueryData(['product', response.id]) as ProductsResponseType
-      const previousProducts = queryClient.getQueryData(['products']) as ProductsResponseType
-      const productToUpdateIndex = previousProducts.data.findIndex((product) => product.id === response.id)
-      const productToUpdate = previousProducts.data[productToUpdateIndex]
-      const updatedProduct = updateObjectValues({itemToUpdate: productToUpdate, partiallyUpdatedItem: response})
-
-      if (updatedProduct) {
-        previousProducts.data[productToUpdateIndex] = updatedProduct
-        queryClient.setQueryData(['products'], (old: ProductsResponseType | undefined) => ({
-          data: [...previousProducts?.data],
-          error: old?.error
-        }))
-
-        queryClient.setQueryData(['product', updatedProduct.id], (old: ProductResponseType | undefined) => ({
-          data: updatedProduct,
-          error: old?.error
-        }))
-      }
-
-      return { previousProduct }
-    },
     onSuccess: async (response: AsyncReturnType<typeof updateProductWithThumbnail>) => {
       queryClient.invalidateQueries({ queryKey: ["products"] });
       queryClient.invalidateQueries({ queryKey: ["product", response.data?.id] });
