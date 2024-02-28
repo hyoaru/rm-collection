bd86bfbc26501f2046e6a3140c46fdb392144453
hyoaru <jjcabreraaaa@gmail.com>
2024-02-28 09:56:41 +0800

    [add] logging update product

--- client/app/_services/admin/operations/updateProductWithThumbnail.ts
+++ client/app/_services/admin/operations/updateProductWithThumbnail.ts
@@ -1,5 +1,6 @@
 import updateProduct from "@services/admin/operations/updateProduct";
 import updateProductThumbnail from "@services/admin/operations/updateProductThumbnail";
+import logAdminAction from "../shared/logAdminAction";
 
 type UpdateProductWithThumbnailParams = {
   id: string;
@@ -21,22 +22,43 @@ export default async function updateProductWithThumbnail({
     name: name,
     description: description,
     category: category,
-  }).then(async ({ data: updateProductData, error: updateProductError }) => {
-    if (updateProductError || !updateProductData) {
-      return { data: updateProductData, error: updateProductError };
-    }
+  })
+    .then(async ({ data: updateProductData, error: updateProductError }) => {
+      if (updateProductError || !updateProductData) {
+        return { data: updateProductData, error: updateProductError };
+      }
 
-    if (thumbnail) {
-      const { error } = await updateProductThumbnail({
-        productId: id,
-        thumbnail: thumbnail,
+      if (thumbnail) {
+        const { error } = await updateProductThumbnail({
+          productId: id,
+          thumbnail: thumbnail,
+        });
+
+        return { data: updateProductData, error };
+      }
+
+      return { data: updateProductData, error: updateProductError };
+    })
+    .then(async ({ data: updateProductData, error: updateProductError }) => {
+      if (updateProductError || !updateProductData) {
+        return { data: updateProductData, error: updateProductError };
+      }
+
+      const details = {
+        id,
+        name,
+        description,
+        category,
+        thumbnail: thumbnail?.[0]?.name,
+      };
+
+      await logAdminAction({
+        action: "update product",
+        details: JSON.stringify(details),
       });
 
-      return { data: updateProductData, error };
-    }
-
-    return { data: updateProductData, error: updateProductError };
-  });
+      return { data: updateProductData, error: updateProductError };
+    });
 
   return { data, error };
 }
