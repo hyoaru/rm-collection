e91e85cc067bf0f2cab2a14977a5beddabe7faa2
hyoaru <jjcabreraaaa@gmail.com>
2024-02-28 10:18:29 +0800

    [add] logging removing admin authority

--- client/app/_services/admin/tables/removeAdminAuthority.ts
+++ client/app/_services/admin/tables/removeAdminAuthority.ts
@@ -3,36 +3,48 @@ import { getUserStateServer } from "@services/authentication/getUserStateServer"
 import { ADMIN_ROLES as adminRoles } from "@constants/admin/base";
 import processErrorToCrossSideSafe from "@lib/processErrorToCrossSideSafe";
 import { Tables } from "@constants/base/database-types";
+import logAdminAction from "@services/admin/shared/logAdminAction";
 
-function getAdminLevelPrecedence(role: typeof adminRoles[number]) {
-  return adminRoles.findIndex((adminRole) => adminRole === role)
+function getAdminLevelPrecedence(role: (typeof adminRoles)[number]) {
+  return adminRoles.findIndex((adminRole) => adminRole === role);
 }
 
-export default async function removeAdminAuthority(targetUser: Tables<'users'>) {
+export default async function removeAdminAuthority(targetUser: Tables<"users">) {
   const supabase = getBrowserClient();
   const authenticatedUser = await getUserStateServer();
 
-  if (!authenticatedUser || authenticatedUser.role === 'user') {
+  if (!authenticatedUser || authenticatedUser.role === "user") {
     return { data: null, error: { message: "User unauthorized" } };
   }
 
-  const authenticatedUserAdminLevel = getAdminLevelPrecedence(authenticatedUser.role)
-  const targetUserAdminLevel = getAdminLevelPrecedence(targetUser?.role)
-  const isTargetUserOfHigherAdminLevel = authenticatedUserAdminLevel >= targetUserAdminLevel
+  const authenticatedUserAdminLevel = getAdminLevelPrecedence(authenticatedUser.role);
+  const targetUserAdminLevel = getAdminLevelPrecedence(targetUser?.role);
+  const isTargetUserOfHigherAdminLevel = authenticatedUserAdminLevel >= targetUserAdminLevel;
 
   if (isTargetUserOfHigherAdminLevel) {
     return {
       data: null,
-      error: { message: "Operation must be done by someone of higher authority." }
-    }
+      error: { message: "Operation must be done by someone of higher authority." },
+    };
   } else {
     const { data, error } = await supabase
-      .from('users')
-      .update({ role: 'user' })
-      .eq('id', targetUser.id)
+      .from("users")
+      .update({ role: "user" })
+      .eq("id", targetUser.id)
       .select()
+      .then(async ({ data, error }) => {
+        if (error || !data) {
+          return { data, error };
+        }
 
-    return { data, error: processErrorToCrossSideSafe(error) }
+        await logAdminAction({
+          action: "remove admin authority",
+          details: JSON.stringify(targetUser),
+        });
+
+        return { data, error };
+      });
+
+    return { data, error: processErrorToCrossSideSafe(error) };
   }
-
 }
