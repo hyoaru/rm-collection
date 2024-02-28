09716688dfaae604dfb49a2bc6aed3287773cc03
hyoaru <jjcabreraaaa@gmail.com>
2024-02-28 10:34:21 +0800

    [add] logging updating product visibility

--- client/app/_services/admin/tables/showProductVariant.ts
+++ client/app/_services/admin/tables/showProductVariant.ts
@@ -1,10 +1,27 @@
 import { Tables } from "@constants/base/database-types";
 import setProductVariantVisibility from "@services/admin/tables/setProductVariantVisibility";
+import logAdminAction from "@services/admin/shared/logAdminAction";
 
 export default async function showProductVariant(productVariant: Tables<"product_variants">) {
   const { data, error } = await setProductVariantVisibility({
     variantId: productVariant.id,
     isVisible: true,
+  }).then(async ({ data, error }) => {
+    if (error || !data) {
+      return { data, error };
+    }
+
+    const details = {
+      isVisible: true,
+      productVariant,
+    };
+
+    await logAdminAction({
+      action: "show product variant",
+      details: JSON.stringify(details),
+    });
+
+    return { data, error };
   });
 
   return { data, error };
