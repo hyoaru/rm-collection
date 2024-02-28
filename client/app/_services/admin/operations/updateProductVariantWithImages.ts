030b319ad3a4c96e7654b59ccd21bcf8797c5262
hyoaru <jjcabreraaaa@gmail.com>
2024-02-28 10:01:06 +0800

    [add] logging update product variant

--- client/app/_services/admin/operations/updateProductVariantWithImages.ts
+++ client/app/_services/admin/operations/updateProductVariantWithImages.ts
@@ -1,5 +1,6 @@
 import updateProductVariant from "@services/admin/operations/updateProductVariant";
 import updateProductVariantImages from "@services/admin/operations/updateProductVariantImages";
+import logAdminAction from "@services/admin/shared/logAdminAction";
 
 type UpdateProductVariantWithImagesParams = {
   product: { id: string };
@@ -29,23 +30,44 @@ export default async function updateProductVariantWithImages({
     quantity: productVariant.quantity,
     price: productVariant.price,
     discountRate: productVariant.discountRate,
-  }).then(async ({ data: updateProductVariantData, error: updateProductVariantError }) => {
-    if (updateProductVariantError || !updateProductVariantData) {
-      return { data: updateProductVariantData, error: updateProductVariantError };
-    }
+  })
+    .then(async ({ data: updateProductVariantData, error: updateProductVariantError }) => {
+      if (updateProductVariantError || !updateProductVariantData) {
+        return { data: updateProductVariantData, error: updateProductVariantError };
+      }
 
-    if (productVariant.images) {
-      const { data, error } = await updateProductVariantImages({
-        images: productVariant.images,
-        productId: product.id,
-        variantId: productVariant.id,
+      if (productVariant.images) {
+        const { data, error } = await updateProductVariantImages({
+          images: productVariant.images,
+          productId: product.id,
+          variantId: productVariant.id,
+        });
+
+        return { data: updateProductVariantData, error };
+      }
+
+      return { data: updateProductVariantData, error: updateProductVariantError };
+    })
+    .then(async ({ data: updateProductVariantData, error: updateProductVariantError }) => {
+      if (updateProductVariantError || !updateProductVariantData) {
+        return { data: updateProductVariantData, error: updateProductVariantError };
+      }
+
+      const details = {
+        product,
+        productVariant: {
+          ...productVariant,
+          images: productVariant?.images ? Array.from(productVariant.images).map((file) => file.name) : null,
+        },
+      };
+
+      await logAdminAction({
+        action: "update product variant",
+        details: JSON.stringify(details),
       });
 
-      return { data: updateProductVariantData, error };
-    }
-
-    return { data: updateProductVariantData, error: updateProductVariantError };
-  });
+      return { data: updateProductVariantData, error: updateProductVariantError };
+    });
 
   return { data, error };
 }
