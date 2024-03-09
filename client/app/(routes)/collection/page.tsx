365485f6c96036a2d94ffe8c3f3b4dce0d6a1503
hyoaru <jjcabreraaaa@gmail.com>
2024-03-09 23:06:55 +0800

    [add] prefetching

--- client/app/(routes)/collection/page.tsx
+++ client/app/(routes)/collection/page.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import getQueryClient from "@services/shared/getQueryClient";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 
 // App imports
-import InfiniteProductsFeed from '@components/collection/shared/InfiniteProductsFeed'
+import InfiniteProductsFeed from "@components/collection/shared/InfiniteProductsFeed";
+import { queryCollectionInfinite } from "@constants/collection/queries";
 
-export default function Page() {
+export default async function Page() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchInfiniteQuery(queryCollectionInfinite({category: 'all'}));
+
   return (
     <>
-      <InfiniteProductsFeed category='all' />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <InfiniteProductsFeed category="all" />
+      </HydrationBoundary>
     </>
-  )
+  );
 }
