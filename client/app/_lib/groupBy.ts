6d54693e0dfaa503ebde47da80c1c7c2f5b3e319
hyoaru <jjcabreraaaa@gmail.com>
2024-03-17 19:16:38 +0800

    [add] admin dashboard

---
+++ client/app/_lib/groupBy.ts
@@ -0,0 +1,7 @@
+export const groupBy = <T, K extends keyof any>(array: T[], getKey: (item: T) => K) =>
+  array.reduce((acc, current) => {
+    const group = getKey(current);
+    if (!acc[group]) acc[group] = [];
+    acc[group].push(current);
+    return acc;
+  }, {} as Record<K, T[]>);
