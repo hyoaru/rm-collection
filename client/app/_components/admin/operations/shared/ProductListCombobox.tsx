b27c2d4c7d62dd6ae168838620240ede044ba8cc
hyoaru <jjcabreraaaa@gmail.com>
2024-03-18 15:23:25 +0800

    [add] revalidate data button

--- client/app/_components/admin/operations/shared/ProductListCombobox.tsx
+++ client/app/_components/admin/operations/shared/ProductListCombobox.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { Check, ChevronsUpDown, RotateCw } from "lucide-react";
 
 // App imports
 import { cn } from "@lib/utils";
@@ -12,6 +12,7 @@ import { Button } from "@components/ui/button";
 import { Skeleton } from "@components/ui/skeleton";
 import { queryAllProducts } from "@constants/shared/queries";
 import { Tables } from "@constants/base/database-types";
+import { useToast } from "@components/ui/use-toast";
 
 type ProductListComboboxProps = {
   value: string | null | undefined;
@@ -22,6 +23,17 @@ type ProductListComboboxProps = {
 export default function ProductListCombobox({ value, setValue, onSelectedValueChange }: ProductListComboboxProps) {
   const [open, setOpen] = useState(false);
   const { data: products, isPending, isFetching } = useQuery(queryAllProducts());
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+
+  async function revalidateData() {
+    await queryClient.invalidateQueries({ queryKey: ["products"], refetchType: "all" });
+
+    toast({
+      title: "Revalidated data.",
+      description: "Changes should take effect immediately.",
+    });
+  }
 
   if (isPending || isFetching) {
     return <Skeleton className="w-full h-10 rounded-lg block" />;
@@ -29,50 +41,55 @@ export default function ProductListCombobox({ value, setValue, onSelectedValueCh
 
   return (
     <>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className="w-full justify-between overflow-hidden"
-          >
-            {value ? products?.data?.find((product) => product.id === value)?.name : "Select product..."}
-            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[325px] sm:w-[200px] lg:w-[400px] xl:w-[650px] p-1">
-          <Command
-            filter={(value, search) => {
-              const productFromValue = products?.data?.find(
-                (product) => product.id.toLowerCase() === value.toLowerCase()
-              );
-              const stringToSearch = productFromValue ? Object.values(productFromValue).join(" ").toLowerCase() : "";
-              if (stringToSearch.includes(search)) return 1;
-              return 0;
-            }}
-          >
-            <CommandInput placeholder="Search product..." />
-            <CommandEmpty>No product found.</CommandEmpty>
-            <CommandGroup className="h-[200px] overflow-y-auto">
-              {products?.data?.map((product) => (
-                <CommandItem
-                  key={`ProductCombobox-${product.id}`}
-                  value={product.id}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? null : currentValue);
-                    onSelectedValueChange(currentValue === value ? null : product);
-                    setOpen(false);
-                  }}
-                >
-                  <Check className={cn("mr-2 h-4 w-4", value === product.id ? "opacity-100" : "opacity-0")} />
-                  {product.name}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
+      <div className="flex gap-2 items-center">
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-full justify-between overflow-hidden"
+            >
+              {value ? products?.data?.find((product) => product.id === value)?.name : "Select product..."}
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[325px] sm:w-[200px] lg:w-[400px] xl:w-[650px] p-1">
+            <Command
+              filter={(value, search) => {
+                const productFromValue = products?.data?.find(
+                  (product) => product.id.toLowerCase() === value.toLowerCase()
+                );
+                const stringToSearch = productFromValue ? Object.values(productFromValue).join(" ").toLowerCase() : "";
+                if (stringToSearch.includes(search)) return 1;
+                return 0;
+              }}
+            >
+              <CommandInput placeholder="Search product..." />
+              <CommandEmpty>No product found.</CommandEmpty>
+              <CommandGroup className="h-[200px] overflow-y-auto">
+                {products?.data?.map((product) => (
+                  <CommandItem
+                    key={`ProductCombobox-${product.id}`}
+                    value={product.id}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? null : currentValue);
+                      onSelectedValueChange(currentValue === value ? null : product);
+                      setOpen(false);
+                    }}
+                  >
+                    <Check className={cn("mr-2 h-4 w-4", value === product.id ? "opacity-100" : "opacity-0")} />
+                    {product.name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
+        <Button size={"icon"} variant={"ghost"} type="button" onClick={revalidateData}>
+          <RotateCw size="20" />
+        </Button>
+      </div>
     </>
   );
 }
--- client/app/_components/admin/operations/shared/UserListCombobox.tsx
+++ client/app/_components/admin/operations/shared/UserListCombobox.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 // App imports
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Check, ChevronsUpDown, RotateCw } from "lucide-react";
 import { cn } from "@lib/utils";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "@components/ui/command";
 import { Popover, PopoverContent, PopoverTrigger } from "@components/ui/popover";
@@ -10,6 +10,7 @@ import { Button } from "@components/ui/button";
 import { Tables } from "@constants/base/database-types";
 import { queryAllUsers } from "@constants/admin/queries";
 import { Skeleton } from "@components/ui/skeleton";
+import { useToast } from "@components/ui/use-toast";
 
 type UserListComboboxProps = {
   value: string | null | undefined;
@@ -19,53 +20,67 @@ type UserListComboboxProps = {
 
 export default function UserListCombobox({ value, setValue, onSelectedValueChange }: UserListComboboxProps) {
   const [open, setOpen] = useState(false);
+  const queryClient = useQueryClient();
   const { data: users, isPending, isFetching } = useQuery(queryAllUsers());
+  const { toast } = useToast();
 
   if (isPending || isFetching) {
     return <Skeleton className="w-full h-10 rounded-lg block" />;
   }
 
+  async function revalidateData() {
+    await queryClient.invalidateQueries({ queryKey: ["products", "product_variants"], refetchType: "all" });
+
+    toast({
+      title: "Revalidated data.",
+      description: "Changes should take effect immediately.",
+    });
+  }
+
   return (
     <>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button variant="outline" role="combobox" aria-expanded={open} className="w-full justify-between">
-            {value ? users?.data?.find((user) => user.email === value)?.email : "Select user..."}
-            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[325px] sm:w-[375px] lg:w-[600px] xl:w-[800px] p-1">
-          <Command
-            filter={(value, search) => {
-              const userFromValue = users?.data?.find(
-                (user) => user.id.toLowerCase() === value.toLowerCase()
-              );
-              const stringToSearch = userFromValue ? Object.values(userFromValue).join(" ").toLowerCase() : "";
-              if (stringToSearch.includes(search)) return 1;
-              return 0;
-            }}
-          >
-            <CommandInput placeholder="Search user..." />
-            <CommandEmpty>No user found.</CommandEmpty>
-            <CommandGroup className="h-[200px] overflow-y-auto">
-              {users?.data?.map((user) => (
-                <CommandItem
-                  key={user.id}
-                  value={user.email}
-                  onSelect={(currentValue) => {
-                    onSelectedValueChange(currentValue === value ? null : user);
-                    setValue(currentValue === value ? null : currentValue);
-                    setOpen(false);
-                  }}
-                >
-                  <Check className={cn("mr-2 h-4 w-4", value === user.email ? "opacity-100" : "opacity-0")} />
-                  {user.email}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
+      <div className="flex items-center gap-2">
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button variant="outline" role="combobox" aria-expanded={open} className="w-full justify-between">
+              {value ? users?.data?.find((user) => user.email === value)?.email : "Select user..."}
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[325px] sm:w-[375px] lg:w-[600px] xl:w-[800px] p-1">
+            <Command
+              filter={(value, search) => {
+                const userFromValue = users?.data?.find((user) => user.id.toLowerCase() === value.toLowerCase());
+                const stringToSearch = userFromValue ? Object.values(userFromValue).join(" ").toLowerCase() : "";
+                if (stringToSearch.includes(search)) return 1;
+                return 0;
+              }}
+            >
+              <CommandInput placeholder="Search user..." />
+              <CommandEmpty>No user found.</CommandEmpty>
+              <CommandGroup className="h-[200px] overflow-y-auto">
+                {users?.data?.map((user) => (
+                  <CommandItem
+                    key={user.id}
+                    value={user.email}
+                    onSelect={(currentValue) => {
+                      onSelectedValueChange(currentValue === value ? null : user);
+                      setValue(currentValue === value ? null : currentValue);
+                      setOpen(false);
+                    }}
+                  >
+                    <Check className={cn("mr-2 h-4 w-4", value === user.email ? "opacity-100" : "opacity-0")} />
+                    {user.email}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
+        <Button size={"icon"} variant={"ghost"} type="button" onClick={revalidateData}>
+          <RotateCw size="20" />
+        </Button>
+      </div>
     </>
   );
 }
