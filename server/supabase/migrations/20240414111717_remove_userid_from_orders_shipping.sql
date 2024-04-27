4018867cf57d2ab89b85b80da955580ec3cabefe
hyoaru <jjcabreraaaa@gmail.com>
2024-04-27 11:49:33 +0800

    [modify] order status -> order shipping

--- server/supabase/migrations/20240414111717_remove_userid_from_orders_shipping.sql
+++ server/supabase/migrations/20240414111717_remove_userid_from_orders_shipping.sql
@@ -1,8 +1,8 @@
 ALTER TABLE IF EXISTS public.orders_shipping 
 DROP COLUMN IF EXISTS user_id CASCADE;
 
-DROP POLICY IF EXISTS "Allow update operation for order status table based on user id" ON public.orders_shipping;
-CREATE POLICY "Allow update operation for order status table based on user id"
+DROP POLICY IF EXISTS "Allow update operation for order shipping table based on user id" ON public.orders_shipping;
+CREATE POLICY "Allow update operation for order shipping table based on user id"
 ON public.orders_shipping
 FOR UPDATE TO authenticated 
 USING (
@@ -20,8 +20,8 @@ WITH CHECK (
   )
 );
 
-DROP POLICY IF EXISTS "Allow insert operation for order status table based on user id" ON public.orders_shipping;
-CREATE POLICY "Allow insert operation for order status table based on user id"
+DROP POLICY IF EXISTS "Allow insert operation for order shipping table based on user id" ON public.orders_shipping;
+CREATE POLICY "Allow insert operation for order shipping table based on user id"
 ON public.orders_shipping
 FOR INSERT TO authenticated 
 WITH CHECK (
